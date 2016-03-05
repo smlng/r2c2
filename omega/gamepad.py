7f17a4e5a7f8e3d1fb069fd8414172669b8f9756
smlng <s@mlng.net>
2016-03-05 21:44:14 +0100

    python gamepad

---
+++ omega/gamepad.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import argparse
+import sys
+
+def ctl_init(dev):
+    pipe = open(dev, 'r')
+    ready  = false
+    buf = []
+    num_btns = 0
+    num_axis = 0
+    while not ready:
+        buf = pipe.read(8)
+        print "[%02X %02X %02X %02X %02X %02X %02X %02X]" % [ord(buf[0]),ord(buf[1]),ord(buf[2]),ord(buf[3]),ord(buf[4]),ord(buf[5]),ord(buf[6]),ord(buf[7])]
+        if ord(buf[6]) == 0x81:
+            num_btns += 1
+        if ord(buf[6]) == 0x82:
+            num_axis += 1
+        if ord(buf[5]) == 0x80:
+            ready = True
+    print "Found gamepad with %d buttons and %d axis\n" % [num_btns, num_axis]
+
+def main():
+    parser = argparse.ArgumentParser(description='', epilog='')
+    parser.add_argument('-d', '--device', help='Input device',
+                        type=str, required=True)
+    args = vars(parser.parse_args())
+    ctl_init(args['device'])
+
+if __name__ == "__main__":
+    main()
